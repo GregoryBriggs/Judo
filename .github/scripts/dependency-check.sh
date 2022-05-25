f8b95b561da1146a1deb24cb2677832f7a336870
Greg Briggs <gregebriggs@gmail.com>
2022-05-24 21:48:26 -0400

    add dependency-check

--- .github/scripts/dependency-check.sh
+++ .github/scripts/dependency-check.sh
@@ -7,8 +7,14 @@ ls -als
 # update npm 
 sudo npm install -g npm@8.10.0
 
+# print current directory
+ls -als
+
 # install dependency check
 sudo npm install dependency-check
+
+# print current directory
+ls -als
 
 # run at root of the project
 sudo npx dependency-check --scan ../
