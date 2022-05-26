41e0fa7a9c793b124963eec6b261ed397f07dbe4
Greg Briggs <gregebriggs@gmail.com>
2022-05-25 21:56:21 -0400

    Add dependency-check

--- .github/scripts/dependency-check.sh
+++ .github/scripts/dependency-check.sh
@@ -27,6 +27,6 @@ echo "running dependency check"
 
 # run at root of the project
 npx dependency-check --version
-npx dependency-check --scan ./node_modules/package.json
+npx dependency-check --scan ./package.json
 
 exit
